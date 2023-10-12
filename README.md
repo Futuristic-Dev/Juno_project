db1cc91da6390811a52e08a1e512a113ffa00140
Solomon <solomonchamamme@gmail.com>
2023-10-12 13:13:34 +0000

    These are the base files for the website Make sure to not change any

---
+++ README.md
@@ -0,0 +1,1 @@
+# Asha Space Website Repository
---
+++ scripts/components/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+console.log("hello you there!");
+
+function MyComp(){
+    return(
+        <h3>
+            You are the test component
+        </h3>
+    )
+}
+
+export default MyComp
