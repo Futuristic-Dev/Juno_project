1b9f0b42f7053e361bcd9476fbde6d2faf8c5a3f
Solomon <solomonchamamme@gmail.com>
2023-10-15 18:16:28 +0000

    Please check and change the words and pictures

--- dist/App.06dce650.js
+++ dist/App.06dce650.js
@@ -3472,7 +3472,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39317" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45557" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
