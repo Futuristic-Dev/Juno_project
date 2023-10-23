0717ed7aaae3579e2cc44d6f099a218557a801b2
Solomon <solomonchamamme@gmail.com>
2023-10-23 12:46:55 +0000

    made a new file to contain the NavBar javascripts

--- src/sections/NavBar.js
+++ src/sections/NavBar.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import {ReactComponent as PhoneInTalk} from '../SVG/phone_in_talk.svg'
 function NavBar() {
   return (
-    <div className='nav'>NavBar</div>
+    <div className='nav'>
+      <div className='nav-bar'>
+            <img src='./logo.png' className='nav-bar--logo'></img>
+          <ul className='nav-bar--lists'>
+            <li className='nav-bar--lists__item'><a href='#'>Home</a></li>
+            <li className='nav-bar--lists__item'><a href='#'>Abouts</a></li>
+            <li className='nav-bar--lists__item'><a href='#'>Services</a></li>
+            <li className='nav-bar--lists__item'><a href='#'>Projects</a></li>
+            <li className='nav-bar--lists__item'><a href='#'>Partners</a></li>
+          </ul>
+          <a className='nav-bar--contact' href='#'>
+            <PhoneInTalk className='nav-bar--contact__icon'></PhoneInTalk>
+            <span className='nav-bar--contact__text'>
+            contact Us
+            </span>
+          </a>
+        </div>
+    </div>
   )
 }
 
