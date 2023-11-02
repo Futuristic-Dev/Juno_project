de5b51636fcc1fd10fb00e7737b35c7e240eb8c5
Von-sledge <papaamoah568@gmail.com>
2023-11-02 05:29:08 +0000

    Added: Banner.js and other banner comps

---
+++ src/components/Banner.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Banner.css'; // Import your CSS file
+import './style/Banner';
+
+const Banner = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slides = [
+    '.styles/Banner/code-view.jpg',
+    '.styles/Banner/computer-view.jpg',
+    '.styles/Banner/hardware.jpg',
+    '.styles/Banner/robot-arm.jpg'
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+  };
+
+  return (
+    <div className='banner'>
+      <button onClick={prevSlide} className='control-btn'>Previous</button>
+      <div className='slider-container'>
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`slide ${index === currentSlide ? 'active' : ''}`}
+            style={{ backgroundImage: `url(${slide})` }}
+          ></div>
+        ))}
+      </div>
+      <button onClick={nextSlide} className='control-btn'>Next</button>
+    </div>
+  );
+}
+
+export default Banner;
