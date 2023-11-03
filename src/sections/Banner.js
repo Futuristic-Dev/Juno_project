fc3b1b1c55580e23a74bdcf734318b4699add671
Solomon <solomonchamamme@gmail.com>
2023-11-03 13:41:48 +0000

    Modified: The Banner js

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import ButtonMain from '../components/ButtonMain'
-import HeaderSecondary from '../components/HeaderSecondary'
+import React from "react";
+import ButtonMain from "../components/ButtonMain";
+import HeaderSecondary from "../components/HeaderSecondary";
 function Banner() {
   return (
-    <div className='banner'>
-      <div className='banner-con'>
-      <HeaderSecondary></HeaderSecondary>
-      <h1 className='banner-con--header-main'>
-        building a generation of skilled engineers
-      </h1>
-      <p className='banner-con--paragraph'>
-        This is simply dummy text of the printing setting industry has been the
-        industry's standard
-      </p>
-      <ButtonMain></ButtonMain>
+    <div className="banner">
+      <div className="banner-con">
+        <HeaderSecondary heading="innovation comes to life"></HeaderSecondary>
+        <h1 className="banner-con--header-main">
+          building a generation of skilled engineers
+        </h1>
+        <p
+          className="banner-con--paragraph"
+        >
+          This is simply dummy text of the printing setting industry has been
+          the industry's standard
+        </p>
+        <ButtonMain></ButtonMain>
       </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
