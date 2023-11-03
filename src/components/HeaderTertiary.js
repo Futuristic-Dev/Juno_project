a93ee0fd3567aa50fa4925af5df8095b36999c8c
Solomon <solomonchamamme@gmail.com>
2023-11-03 10:30:42 +0000

    Modified: The heading accross the page

---
+++ src/components/HeaderTertiary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeaderTertiary() {
+  return (
+    <>
+        <h2>
+            leading force in advanced technology
+        </h2>
+    </>
+  )
+}
+
+export default HeaderTertiary
