6e251d4c1c60c4e7b6eed397cf890fc5a223d787
Solomon <solomonchamamme@gmail.com>
2023-11-06 23:00:50 +0000

    Added: The grid layout

--- src/components/AboutGallery.js
+++ src/components/AboutGallery.js
@@ -1,17 +1,50 @@
 import React from "react";
-
+import RigImage from "../styles/layout/rig-image.jpeg";
+import EngImage from "../Asah_Images/Rectangle 10.png";
 function AboutGallery() {
   return (
-    <div className="about-gallery" style={{
-      display:'grid',
-      gridTemplateRows:'repeat(12, 1fr)',
-      gridTemplateColumns:'repeat(12, 1fr)'
-    }}>
-      <img className="about-gallery__img-1" alt="aboutimage"></img>
-      <img className="about-gallery__img-2" alt="aboutimage"></img>
+    <div
+      className="about-gallery"
+      style={{
+        display: "grid",
+        gridTemplateRows: "repeat(12, 0.5fr)",
+        gridTemplateColumns: "repeat(20, 0.5fr)",
+        height:'100%'
+      }}
+    >
+      <img
+        className="about-gallery__img-1"
+        alt="aboutimage"
+        src={RigImage}
+        style={{
+          gridRow: "2 / 15",
+          gridColumn: "4 / 20",
+          width: "100%",
+          height: "86%",
+          borderRadius:'.9rem'
+          
+        }}
+      ></img>
+      <img className="about-gallery__img-2" alt="aboutimage"
+      style={{
+        gridRow: "7 / -1",
+          gridColumn: "2 / 12",
+          width: "100%",
+          height: "86%",
+          borderRadius:'.9rem'
+      }}
+      src={EngImage}
+      ></img>
       <img className="about-gallery__img-3" alt="aboutimage"></img>
       <div className="about-gallery__banner">
-        <span className="about-gallery__banner--number">30</span>
+        <span className="about-gallery__banner-rtt-number"
+        style={{
+          display: "grid",
+          border: "10px dashed #89A0cCD",
+          gridColumn: "4 / 9",
+          gridRow: "11 / 13",
+        }}
+        >30</span>
         <span className="about-gallery__banner--text">years of experience</span>
       </div>
     </div>
