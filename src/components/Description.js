3da0afb506e2cad37538a3c1eb362176e2cd3545
Solomon <solomonchamamme@gmail.com>
2023-11-07 14:03:05 +0000

    Created: A paragraph component

---
+++ src/components/Description.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Description(prop) {
+  const { description } = prop;
+//   console.log(description);
+  return (
+    <>
+      <p
+        style={{
+          fontSize: "1.5rem",
+          fontWeight: "400",
+          padding: "0rem 1rem 0rem 0rem",
+          margin: "1rem 1rem 1.5rem 0rem",
+          lineHeight: "2.1rem",
+        }}
+      >
+        {description}
+      </p>
+    </>
+  );
+}
+
+export default Description;
