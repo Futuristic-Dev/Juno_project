6a712a33dd0d1fc7d81f7f3d08a07bfab30595c1
Solomon <solomonchamamme@gmail.com>
2023-11-10 20:09:34 +0000

    New file

---
+++ src/components/ExpertsCards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import pic from '../Asah_Images/expert_1.png'
+
+function ExpertsCards() {
+  return (
+    <div className='experts-card' style={{
+    }}>
+        <img src={pic} style={{
+            width:'100%',
+            height:'100%',
+            padding:'1rem'
+        }}></img>
+    </div>
+  )
+}
+
+export default ExpertsCards
