892f62b961789dc5ef76a1d1844fbaf2fd39dc70
Solomon <solomonchamamme@gmail.com>
2023-11-10 20:11:18 +0000

    New

--- src/sections/Experts.js
+++ src/sections/Experts.js
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import HeaderSecondary from '../components/HeaderSecondary'
+import HeaderTertiary from '../components/HeaderTertiary'
+import ExpertsCards from '../components/ExpertsCards'
 function Experts() {
   return (
-    <div className='experts'>Experts</div>
+    <div className='experts'
+      style={{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
+      }}
+    >
+      <div style={{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        <HeaderSecondary heading='Our Experts'></HeaderSecondary>
+        <HeaderTertiary heading='Meet Our Experienced Team'></HeaderTertiary>
+      </div>
+      <div>
+        <ExpertsCards></ExpertsCards>
+      </div>
+    </div>
   )
 }
 
