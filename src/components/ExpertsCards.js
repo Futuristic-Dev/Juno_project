633e7733d878a3f9912109da06873792d00303f4
Solomon <solomonchamamme@gmail.com>
2023-11-12 23:36:32 +0000

    Experts

--- src/components/ExpertsCards.js
+++ src/components/ExpertsCards.js
@@ -8,7 +8,7 @@ function ExpertsCards() {
         <img src={pic} style={{
             width:'100%',
             height:'100%',
-            padding:'1rem'
+            padding:'1rem',
         }}></img>
     </div>
   )
