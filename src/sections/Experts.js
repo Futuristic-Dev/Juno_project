1669810ccfbf6eb1cff1d1d4da80abf1b1b0fc15
Solomon <solomonchamamme@gmail.com>
2023-11-13 18:34:41 +0000

    Experts layout

--- src/sections/Experts.js
+++ src/sections/Experts.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import HeaderSecondary from '../components/HeaderSecondary'
 import HeaderTertiary from '../components/HeaderTertiary'
-import ExpertsCards from '../components/ExpertsCards'
+import ExpertsCards from '../components/ExpertsCards';
+import expert_img_3 from '../Asah_Images/expert_1.png'
+import expert_img_2 from '../Asah_Images/expert_2.png'
+import expert_img_1 from '../Asah_Images/expert_3.png'
+
+const expert_info = [{
+  id:'01',
+  img:expert_img_1,
+  name:'Asiedu Emmanuel',
+  position:'Manger',
+},
+{
+  id:'02',
+  img:expert_img_2,
+  name:'Kissiwaa Psalmet',
+  position:'Director',
+},
+{
+  id:'03',
+  img:expert_img_3,
+  name:'Maanu Emmanualla',
+  position:'Secretary',
+}]
+
 function Experts() {
   return (
     <div className='experts'
@@ -21,8 +44,17 @@ function Experts() {
         <HeaderSecondary heading='Our Experts'></HeaderSecondary>
         <HeaderTertiary heading='Meet Our Experienced Team'></HeaderTertiary>
       </div>
-      <div>
-        <ExpertsCards></ExpertsCards>
+      <div style={{
+        display:'grid',
+        gridTemplateColumns: 'repeat(3, 22rem)',
+        gridGap:'1.5rem',
+        // justifyContent:'space-around'
+      }}>
+        {
+          expert_info.map((val, ind) =>{
+            return <ExpertsCards key={ind} list={val}/>
+          })
+        }
       </div>
     </div>
   )
