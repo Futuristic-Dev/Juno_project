d0dc92ca435501c1cef6ea02ba30ff54971634e8
Solomon <solomonchamamme@gmail.com>
2023-11-15 22:14:57 +0000

    Modified

--- src/sections/ChooseUs.js
+++ src/sections/ChooseUs.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import ChooseMain from '../components/ChooseMain'
+import ChooseGallery from '../components/ChooseGallery'
 
 function ChooseUs() {
   return (
-    <div className='choose'>ChooseUs</div>
+    <div className='choose' style={{
+      display:'grid',
+      gridTemplateColumns:'repeat(2, 1fr)',
+      padding:'4rem'
+
+    }}>
+      <ChooseMain></ChooseMain>
+      <ChooseGallery></ChooseGallery>
+    </div>
   )
 }
 
