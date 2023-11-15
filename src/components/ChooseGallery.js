25557194dab1b1e9b9cc24c0db32af3e864816d4
Solomon <solomonchamamme@gmail.com>
2023-11-15 22:14:29 +0000

    A Gallery style right there

---
+++ src/components/ChooseGallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import wave_1 from '../SVG/wave(1).svg'
+
+function ChooseGallery() {
+  return (
+    <div>
+        {/* <img src={wave_1}></img> */}
+    </div>
+  )
+}
+
+export default ChooseGallery
