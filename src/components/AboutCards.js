ca058f543c6409af06c03ef3b046e1dcf529534e
Solomon <solomonchamamme@gmail.com>
2023-11-15 22:13:30 +0000

    Altered

--- src/components/AboutCards.js
+++ src/components/AboutCards.js
@@ -1,20 +1,31 @@
-import React from "react";
-import { ReactComponent as Engineering } from "../SVG/engineering.svg";
+import React ,{useState}from "react";
+// import { ReactComponent as Engineering } from "../SVG/engineering.svg";
 
-function AboutCards() {
+function AboutCards(props) {
+  const {info} = props;
+
+  // console.log(info)
+  const [hover, setHover] = useState(false);
+
+    const handleMouseEnter = () =>{
+        setHover(true)
+    }
+
+    const handleMouseLeave = () =>{
+        setHover(false)
+    }
   return (
-    <div className="about-cards">
+    <div className="about-cards" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="about-cards__holder">
         <div className="about-cards__holder--icon">
-        <Engineering className="about-cards__icon"></Engineering>
+          {/* <Engineering className="about-cards__icon"></Engineering> */}
+          <img src={info.icon} className="about-cards__icon"></img>
         </div>
-        <span className="about-cards__holder--number">01</span>
+        <span className="about-cards__holder--number">{info.id}</span>
       </div>
-      <h4 className="about-cards__heading">emergency services</h4>
+      <h4 className="about-cards__heading">{info.title}</h4>
       <p className="about-cards__paragraph">
-        This is simply dummy text of the printing typesetting industry has been
-        industry's standard dummy text ever since when an unknown printer took a
-        galley.
+        {info.description}
       </p>
     </div>
   );
